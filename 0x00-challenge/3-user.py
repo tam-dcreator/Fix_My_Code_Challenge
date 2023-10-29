f152c1589745641630a0748909798f470bdaa974
tam-dcreator <tam@THE-7TH>
2023-10-29 23:40:11 +0100

    Fixed the User class, yet again

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -6,7 +6,7 @@ import hashlib
 import uuid
 
 
-class User:
+class User():
     """
     User class:
     - id: public string unique (uuid)
@@ -18,7 +18,7 @@ class User:
     def __init__(self):
         """
         Initialize a new user:
-        - assigned a unique `id`
+        - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
 
@@ -40,7 +40,7 @@ class User:
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
